3d3dd27a5d744b2518dd0231d7b5029481ce17e1
Bartosz Walicki <walicki.bartosz@gmail.com>
2021-06-28 00:25:30 +0200

    PORT fix.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use("/static", express.static(__dirname + "/static"));
 
-var server = app.listen(5000);
+var server = app.listen(PORT);
